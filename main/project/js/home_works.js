a86d6e06f537ee6cfc6f61d7a15e2ac3b5b75b32
Chyngyz <142288166+AlmazovChyngyz@users.noreply.github.com>
2023-11-14 23:05:43 +0600

    Second H/W

--- main/project/js/home_works.js
+++ main/project/js/home_works.js
@@ -17,17 +17,83 @@ gmailButton.addEventListener("click", () => {
 })
 
 
-//HOME WORK 1 (PART2)
+// HOME WORK 1/2 (PART2)
 
-let position = 0;
+const childBlock = document.querySelector(".child_block");
 
-function recursionAnimation(){
-    position = position + 5;
-    if (position > 450) return;
-    document.querySelector(".child_block").style.left = position +"px";
-    animation();
+let positionX = 0;
+let positionY = 0;
+
+const moveChildBlock = () => {
+    if (positionX < 843 && positionY === 0) {
+        positionX++;
+        childBlock.style.left = `${positionX}px`;
+    } else if (positionX >= 843 && positionY < 546) {
+        positionY++;
+        childBlock.style.top = `${positionY}px`;
+    } else if (positionY >= 448 && positionX > 0) {
+        positionX--;
+        childBlock.style.left = `${positionX}px`;
+    } else if (positionX === 0 && positionY > 0) {
+        positionY--;
+        childBlock.style.top = `${positionY}px`;
+    }
+    setTimeout(moveChildBlock, 5);
+};
+moveChildBlock();
+
+
+// let position = 0;
+//
+// function recursionAnimation(){
+//     position = position + 5;
+//     if (position > 450) return;
+//     document.querySelector(".child_block").style.left = position +"px";
+//     animation();
+// }
+// function animation(){
+//     setTimeout(recursionAnimation, 100);
+// }
+// animation();
+
+
+// HOME WORK 2
+
+const seconds = document.getElementById('secondsS');
+const start = document.getElementById('start');
+const stop = document.getElementById('stop');
+const reset = document.getElementById('reset');
+
+start.addEventListener('click', startCounter);
+stop.addEventListener('click', stopCounter);
+reset.addEventListener('click', resetCounter);
+
+let counterValue = 0;
+let intervalId;
+function updateCounter() {
+    seconds.textContent = counterValue;
+    counterValue++;
 }
-function animation(){
-    setTimeout(recursionAnimation, 100);
+
+function startCounter() {
+    intervalId = setInterval(updateCounter, 1000);
+    start.disabled = true;
+    stop.disabled = false;
+    reset.disabled = false;
 }
-animation();
+
+function stopCounter() {
+    clearInterval(intervalId);
+    start.disabled = false;
+    stop.disabled = true;
+    reset.disabled = false;
+}
+
+function resetCounter() {
+    clearInterval(intervalId);
+    counterValue = 0;
+    seconds.textContent = counterValue;
+    start.disabled = false;
+    stop.disabled = true;
+    reset.disabled = true;
+}
