8157b7662b7b59dd6d64b0d7d5a167bc0fe9e752
Chyngyz <142288166+AlmazovChyngyz@users.noreply.github.com>
2023-11-17 20:42:41 +0600

    Third H/W

--- main/project/js/lesson.js
+++ main/project/js/lesson.js
@@ -15,3 +15,51 @@ phoneButton.addEventListener("click", () =>{
         phoneSpan.style.color = "red";
     }
 })
+
+//Tab slider
+
+const tabsContentCards = document.querySelectorAll('.tab_content_block')
+const tabsItems = document.querySelectorAll('.tab_content_item')
+const tabsItemsParent = document.querySelector('.tab_content_items')
+
+let index = 0
+const hideTabsContentCards = () => {
+    tabsContentCards.forEach((tabContentCard) => {
+        tabContentCard.style.display = 'none'
+    })
+    tabsItems.forEach((tabItem) =>{
+        tabItem.classList.remove('tab_content_item_active')
+    })
+}
+// hideTabsContentCards()
+
+const showTabsContentCards = (indexElement = 0) => {
+    tabsContentCards[indexElement].style.display = 'block'
+    tabsItems[indexElement].classList.add('tab_content_item_active')
+}
+
+const changeTabAuto = () => {
+    hideTabsContentCards()
+    showTabsContentCards(index)
+
+    index= (index+1) % tabsContentCards.length
+}
+
+hideTabsContentCards()
+showTabsContentCards(0)
+
+const intervalId = setInterval(changeTabAuto, 3000)
+
+tabsItemsParent.onclick = (event) => {
+    if (event.target.classList.contains('tab_content_item')) {
+        tabsItems.forEach((tabItem, tabItemIndex) => {
+            if (event.target === tabItem) {
+                hideTabsContentCards()
+                showTabsContentCards(tabItemIndex)
+
+                clearInterval(intervalId)
+            }
+        })
+    }
+}
+
